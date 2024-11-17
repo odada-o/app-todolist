ddf95b57d75198b89518d13339ac2bb24951db00
oddoddo <eehd80yul@gmail.com>
2024-11-17 22:24:40 +0900

    useMemo

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -18,6 +18,17 @@ const TodoList = () => {
         return todos.filter((item) => item.task.toLowerCase().includes(search.toLowerCase()));
     }, [todos, search]);
 
+    // lookBack 함수가 빈번하게 호출됩니다.
+    // 최적화 테스트
+    const lookBack = useMemo(() => {
+        console.log('lookBack')
+        const total = todos.length
+        const done = todos.filter((item) => item.isDone).length
+        const left = total - done
+
+        return { total, done, left }
+    }, [todos]) // 의존성 배열에 todo를 추가합니다.
+
 
     // TodoItem에서 사용할 핸들러를 useCallback으로 메모이제이션
     const handleUpdate = useCallback((id) => onUpdate(id), [onUpdate]);
@@ -31,15 +42,18 @@ const TodoList = () => {
             onChange={onChangeSearch}
             placeholder="검색어를 입력하세요"/>
 
+            <div>
+                {lookBack.total}개 중에 {lookBack.done}개 완료, {lookBack.left}개 남음
+            </div>
+
             <ul>
-                {filteredTodo().map((todo) => (
+                {filteredTodo.map((todo) => (
                     <TodoItem 
                     key={todo.id} 
                     onUpdate={() => handleUpdate(todo.id)} // 메모이제이션된 업데이트 핸들러
                     onDelete={() => handleDelete(todo.id)} // 메모이제이션된 삭제 핸들러 {...todo} 
                     />
                 ))}
-                
             </ul>
         </div>
     );
